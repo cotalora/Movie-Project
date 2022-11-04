7d1d14bcce3754261b8156d59cdb3fe8be860f70
cotalora <67478427+cotalora@users.noreply.github.com>
2022-11-04 09:22:28 -0500

    Ajustes a despligue

--- src/main.tsx
+++ src/main.tsx
@@ -3,15 +3,15 @@ import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux';
 import { store } from './store';
 import { App } from './App';
-import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter, HashRouter } from 'react-router-dom';
 import './index.scss';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <Provider store={store}>
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
+        <HashRouter>
+          <App />
+        </HashRouter>
     </Provider>
   </React.StrictMode>
 )
