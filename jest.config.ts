803b3467b820b36a507322de84c6cf804b0af437
cotalora <67478427+cotalora@users.noreply.github.com>
2022-11-04 09:22:43 -0500

    Ajustes a despligue

---
+++ jest.config.ts
@@ -0,0 +1,17 @@
+export default {
+    testEnvironment: "jsdom",
+    transform: {
+        "^.+\\.tsx?$": "ts-jest"
+    },
+    transformIgnorePatterns: [
+        'node_modules/'
+    ],
+    moduleNameMapper: {
+        '\\.(gif|ttf|eot|svg|png)$': '<rootDir>/test/__mocks__/fileMock.js',
+        '\\.(css|less|sass|scss)$': 'identity-obj-proxy',
+    },
+    setupFiles: ['jest-canvas-mock'],
+    setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
+    collectCoverage: true,
+    coverageReporters: ["json", "html"]
+}
